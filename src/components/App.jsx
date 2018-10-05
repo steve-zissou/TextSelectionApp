3e9e44013967fe0fce9860cbcb76891ff67c1ada
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-05 22:30:11 +0100

    Added code required to get basic application launchable.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+// 3rd Party
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {"This is the beginning!"}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// 3rd Party
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Custom
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
