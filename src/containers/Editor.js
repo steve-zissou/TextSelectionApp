e81595f6b1d66544f577096c5bed8d8eccc90bef
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-06 17:09:17 +0100

    Add Redux containers to send and display highlights from state

---
+++ src/containers/Editor.js
@@ -0,0 +1,18 @@
+// 3rd Party
+import { connect } from 'react-redux';
+// Custom
+import { addHighlightAction } from '../actions/highlights';
+import Editor from '../components/Editor';
+
+
+function mapStateToProps() {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick: text => dispatch(addHighlightAction(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Editor);
