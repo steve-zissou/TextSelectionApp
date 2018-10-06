4185547c4615a4bf86e749887c4af1d1bfaebd9b
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-06 17:07:16 +0100

    Fixes to silence unwanted linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     "env": {
+        "browser": true,
         "jest": true,
+        "node": true,
     },
     "extends": "airbnb",
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint react/jsx-filename-extension: 0 */
 // 3rd Party
 import React from 'react';
 import ReactDOM from 'react-dom';
