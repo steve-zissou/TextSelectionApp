fc3c2fe9cead50cb1a74b111ebb6956fb5dfd766
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-08 12:33:27 +0100

    Added docstrings to Draft Js utils

--- src/utils/draft-js.js
+++ src/utils/draft-js.js
@@ -1,5 +1,15 @@
 /* eslint import/prefer-default-export: 0 */
+/**
+ * Helper functions for working with DraftJs.
+ * Based on: // https://github.com/facebook/draft-js/issues/442
+ */
 
+/**
+ * Get the blocks which are between the supplied start and end block keys.
+ * @param {[ContentBlock]} blocks The array of all the blocks.
+ * @param {string} startKey The key of the first block.
+ * @param {string} endKey The key of the last block.
+ */
 function getBlocksBetween(blocks, startKey, endKey) {
   let lastWasEnd = false;
 
@@ -12,6 +22,15 @@ function getBlocksBetween(blocks, startKey, endKey) {
     });
 }
 
+/**
+ * Join the text from the start offset of the start block to the end offset of the end block.
+ * @param {[ContentBlock]} blocks The array of all the blocks.
+ * @param {string} startKey The key of the first block.
+ * @param {string} endKey The key of the last block.
+ * @param {int} startOffset The text offset within the first block.
+ * @param {int} endOffset The text offset within the last block.
+ * @returns {string}
+ */
 function joinBlockText(blocks, startKey, endKey, startOffset, endOffset) {
   return blocks
     .map((block) => {
@@ -24,7 +43,12 @@ function joinBlockText(blocks, startKey, endKey, startOffset, endOffset) {
     .join('\n');
 }
 
-// https://github.com/facebook/draft-js/issues/442
+/**
+ * Get the current selected text.
+ * @param {ContentState} content The current content state.
+ * @param {SelectionState} selection The current selection state.
+ * @returns {string}
+ */
 export function getTextSelection(content, selection) {
   const startKey = selection.getStartKey();
   const endKey = selection.getEndKey();
@@ -36,6 +60,13 @@ export function getTextSelection(content, selection) {
   );
 }
 
+/**
+ * Check if a block is within a range of blocks.
+ * @param {ContentState} content The current content state.
+ * @param {ContentBlock} block The block to check.
+ * @param {string} startKey The key of the first block.
+ * @param {string} endKey The key of the last block.
+ */
 export function isBlockInRange(content, block, startKey, endKey) {
   const allBlocks = content.getBlockMap();
   const blocks = getBlocksBetween(allBlocks, startKey, endKey);
@@ -43,6 +74,11 @@ export function isBlockInRange(content, block, startKey, endKey) {
   return blockIds.includes(block.getKey());
 }
 
+/**
+ * Check if a selection is valid (not empty).
+ * @param {string} text The selection text to check.
+ * @returns {bool}
+ */
 export function isValidSelection(text) {
   return text.replace(/(?:\r\n|\r|\n|\s)/g, '').length > 0;
 }
