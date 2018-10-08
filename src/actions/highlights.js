81612ea0cff4b3841e9eb199c27a4a2ca35ec3d0
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-08 12:53:11 +0100

    Added action docstring

--- src/actions/highlights.js
+++ src/actions/highlights.js
@@ -4,6 +4,11 @@ import uuid from 'uuid/v1';
 
 export const ADD_HIGHLIGHT = 'ADD_HIGHLIGHT';
 
+/**
+ * Defines the shape of the action to add a highlight to the application state.
+ * @param {object} selection The user selection to add.
+ * @returns {object}
+ */
 export function addHighlightAction(selection) {
   const {
     endKey, endOffset, startKey, startOffset, text,
